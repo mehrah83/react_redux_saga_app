ac9acd86d9e423552ca151c4e76679118c00e684
Himanshu Mehra <92850875+mehrah83@users.noreply.github.com>
2022-09-29 23:53:55 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import "./components/Responsive.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import { Routes, Route } from "react-router-dom";
 import Cart from "./components/Cart";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
@@ -12,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Main />} />
         <Route path="/cart" element={<Cart/>} />
       </Routes>
+      <Footer/>
     </>
   );
 };
